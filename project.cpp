6b6de1b7c8d68e9e5241df144bab8317d15ac0db
vhborges <viniciusborges22@gmail.com>
2018-09-11 23:21:56 -0300

    exhaust all possibilities

--- project.cpp
+++ project.cpp
@@ -11,7 +11,7 @@ void print_solution(const std::vector<std::vector<int>>& board)
     }
     std::cout << "\n";
   }
-  std::cout << "\n";
+  std::cout << std::endl;
 }
 
 bool is_valid_up(const std::vector<std::vector<int>>& board, std::size_t i, std::size_t j)
@@ -46,44 +46,58 @@ bool is_valid_left(const std::vector<std::vector<int>>& board, std::size_t i, st
     return false;
 }
 
-void find_solutions(std::vector<std::vector<int>> board, int count)
+void find_solutions(std::vector<std::vector<int>> board, int count, std::size_t i, std::size_t j)
 {
   if(count == 1)
   {
     print_solution(board);
     return;
   }
-  for(std::size_t i = 0; i < board.size(); i++)
+  if(board[i][j] != 0)
   {
-    for(std::size_t j = 0; j < board[i].size(); j++)
+    if(is_valid_up(board, i, j))
     {
-      if(board[i][j] != 0)
-      {
-        if(is_valid_up(board, i, j))
-        {
-          board[i-1][j] = board[i][j];
-          board[i][j] = 0;
-          find_solutions(board, count-1);
-        }
-        if(is_valid_right(board, i, j))
-        {
-          board[i][j+1] = board[i][j];
-          board[i][j] = 0;
-          find_solutions(board, count-1);
-        }
-        if(is_valid_down(board, i, j))
-        {
-          board[i+1][j] = board[i][j];
-          board[i][j] = 0;
-          find_solutions(board, count-1);
-        }
-        if(is_valid_left(board, i, j))
-        {
-          board[i][j-1] = board[i][j];
-          board[i][j] = 0;
-          find_solutions(board, count-1);
-        }
-      }
+      int aux = board[i-1][j];
+      board[i-1][j] = board[i][j];
+      board[i][j] = 0;
+      for(std::size_t _i = 0; _i < board.size(); _i++)
+        for(std::size_t _j = 0; _j < board[j].size(); _j++)
+          find_solutions(board, count-1, _i, _j);
+      board[i][j] = board[i-1][j];
+      board[i-1][j] = aux;
+    }
+    if(is_valid_right(board, i, j))
+    {
+      int aux = board[i][j+1];
+      board[i][j+1] = board[i][j];
+      board[i][j] = 0;
+      for(std::size_t _i = 0; _i < board.size(); _i++)
+        for(std::size_t _j = 0; _j < board[j].size(); _j++)
+          find_solutions(board, count-1, _i, _j);
+      board[i][j] = board[i][j+1];
+      board[i][j+1] = aux;
+    }
+    if(is_valid_down(board, i, j))
+    {
+      int aux = board[i+1][j];
+      board[i+1][j] = board[i][j];
+      board[i][j] = 0;
+      for(std::size_t _i = 0; _i < board.size(); _i++)
+        for(std::size_t _j = 0; _j < board[j].size(); _j++)
+          find_solutions(board, count-1, _i, _j);
+      board[i][j] = board[i+1][j];
+      board[i+1][j] = aux;
+    }
+    if(is_valid_left(board, i, j))
+    {
+      int aux = board[i][j-1];
+      board[i][j-1] = board[i][j];
+      board[i][j] = 0;
+      for(std::size_t _i = 0; _i < board.size(); _i++)
+        for(std::size_t _j = 0; _j < board[j].size(); _j++)
+          find_solutions(board, count-1, _i, _j);
+      board[i][j] = board[i][j-1];
+      board[i][j-1] = aux;
     }
   }
 }
@@ -104,7 +118,13 @@ int main()
     }
   }
 
-  find_solutions(board, count);
+  for(std::size_t i = 0; i < board.size(); i++)
+  {
+    for(std::size_t j = 0; j < board[i].size(); j++)
+    {
+      find_solutions(board, count, i, j);
+    }
+  }
 
   return 0;
 }
