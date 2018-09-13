c017c5270365e17e40b6db3eec3a13405d8e9c3d
vhborges <viniciusborges22@gmail.com>
2018-09-12 21:08:21 -0300

    fix output format

--- project.cpp
+++ project.cpp
@@ -1,113 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
-void print_solution(const std::vector<std::vector<int>>& board)
+void fill_board(std::vector<std::vector<int>>& board, const int rows, const int columns, int& count)
 {
-  for(auto i : board)
-  {
-    for(auto j : i)
-    {
-      std::cout << j << " ";
-    }
-    std::cout << "\n";
-  }
-  std::cout << std::endl;
-}
-
-bool is_valid_up(const std::vector<std::vector<int>>& board, std::size_t i, std::size_t j)
-{
-  if((int) i-1 >= 0)
-    return (board[i-1][j] == board[i][j] + 1 || (board[i][j] == 3 && board[i-1][j] == 1));
-  else
-    return false;
-}
-
-bool is_valid_right(const std::vector<std::vector<int>>& board, std::size_t i, std::size_t j)
-{
-  if(j+1 < board[i].size())
-    return (board[i][j+1] == board[i][j] + 1 || (board[i][j] == 3 && board[i][j+1] == 1));
-  else
-    return false;
-}
-
-bool is_valid_down(const std::vector<std::vector<int>>& board, std::size_t i, std::size_t j)
-{
-  if(i+1 < board.size())
-    return (board[i+1][j] == board[i][j] + 1 || (board[i][j] == 3 && board[i+1][j] == 1));
-  else
-    return false;
-}
-
-bool is_valid_left(const std::vector<std::vector<int>>& board, std::size_t i, std::size_t j)
-{
-  if((int) j-1 >= 0)
-    return (board[i][j-1] == board[i][j] + 1 || (board[i][j] == 3 && board[i][j-1] == 1));
-  else
-    return false;
-}
-
-void find_solutions(std::vector<std::vector<int>> board, int count, std::size_t i, std::size_t j)
-{
-  if(count == 1)
-  {
-    print_solution(board);
-    return;
-  }
-  if(board[i][j] != 0)
-  {
-    if(is_valid_up(board, i, j))
-    {
-      int aux = board[i-1][j];
-      board[i-1][j] = board[i][j];
-      board[i][j] = 0;
-      for(std::size_t _i = 0; _i < board.size(); _i++)
-        for(std::size_t _j = 0; _j < board[j].size(); _j++)
-          find_solutions(board, count-1, _i, _j);
-      board[i][j] = board[i-1][j];
-      board[i-1][j] = aux;
-    }
-    if(is_valid_right(board, i, j))
-    {
-      int aux = board[i][j+1];
-      board[i][j+1] = board[i][j];
-      board[i][j] = 0;
-      for(std::size_t _i = 0; _i < board.size(); _i++)
-        for(std::size_t _j = 0; _j < board[j].size(); _j++)
-          find_solutions(board, count-1, _i, _j);
-      board[i][j] = board[i][j+1];
-      board[i][j+1] = aux;
-    }
-    if(is_valid_down(board, i, j))
-    {
-      int aux = board[i+1][j];
-      board[i+1][j] = board[i][j];
-      board[i][j] = 0;
-      for(std::size_t _i = 0; _i < board.size(); _i++)
-        for(std::size_t _j = 0; _j < board[j].size(); _j++)
-          find_solutions(board, count-1, _i, _j);
-      board[i][j] = board[i+1][j];
-      board[i+1][j] = aux;
-    }
-    if(is_valid_left(board, i, j))
-    {
-      int aux = board[i][j-1];
-      board[i][j-1] = board[i][j];
-      board[i][j] = 0;
-      for(std::size_t _i = 0; _i < board.size(); _i++)
-        for(std::size_t _j = 0; _j < board[j].size(); _j++)
-          find_solutions(board, count-1, _i, _j);
-      board[i][j] = board[i][j-1];
-      board[i][j-1] = aux;
-    }
-  }
-}
-
-int main()
-{
-  int rows, columns, count = 0;
-  std::cin >> rows >> columns;
-  std::vector<std::vector<int>> board(rows, std::vector<int>(columns));
-
   for(int i = 0; i < rows; i++)
   {
     for(int j = 0; j < columns; j++)
@@ -117,14 +13,178 @@ int main()
         count++;
     }
   }
+}
 
-  for(std::size_t i = 0; i < board.size(); i++)
+void fill_solutions(const std::vector<std::vector<int>>& board, std::set<std::vector<int>>& solutions)
+{
+  for(int i = 0; i < (int) board.size(); i++)
   {
-    for(std::size_t j = 0; j < board[i].size(); j++)
+    for(int j = 0; j < (int) board[j].size(); j++)
     {
-      find_solutions(board, count, i, j);
+      if(board[i][j] != 0)
+        solutions.insert({i+1, j+1, board[i][j]});
     }
   }
+}
+
+void print_solutions(const std::set<std::vector<int>>& solutions, int total_sol)
+{
+  std::cout << total_sol << "\n" << solutions.size() << "\n";
+  for(auto s : solutions)
+  {
+    for(auto v : s)
+    {
+      std::cout << v << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+bool is_valid_up(const std::vector<std::vector<int>>& board, const int i, const int j)
+{
+  if(i-1 >= 0)
+    return (board[i-1][j] == board[i][j] + 1 || (board[i][j] == 3 && board[i-1][j] == 1));
+  else
+    return false;
+}
+
+bool is_valid_right(const std::vector<std::vector<int>>& board, const int i, const int j)
+{
+  if(j+1 < (int) board[i].size())
+    return (board[i][j+1] == board[i][j] + 1 || (board[i][j] == 3 && board[i][j+1] == 1));
+  else
+    return false;
+}
+
+bool is_valid_down(const std::vector<std::vector<int>>& board, const int i, const int j)
+{
+  if(i+1 < (int) board.size())
+    return (board[i+1][j] == board[i][j] + 1 || (board[i][j] == 3 && board[i+1][j] == 1));
+  else
+    return false;
+}
+
+bool is_valid_left(const std::vector<std::vector<int>>& board, const int i, const int j)
+{
+  if(j-1 >= 0)
+    return (board[i][j-1] == board[i][j] + 1 || (board[i][j] == 3 && board[i][j-1] == 1));
+  else
+    return false;
+}
+
+void find_solutions(std::vector<std::vector<int>> board, int count, const int i, const int j, int& total_sol, std::set<std::vector<int>>& solutions)
+{
+  if(count == 1)
+  {
+    total_sol++;
+    fill_solutions(board, solutions);
+    return;
+  }
+  if(board[i][j] != 0)
+  {
+    if(is_valid_up(board, i, j))
+    {
+      const int aux = board[i-1][j];
+      board[i-1][j] = board[i][j];
+      board[i][j] = 0;
+      for(int _i = 0; _i < (int) board.size(); _i++)
+      {
+        for(int _j = 0; _j < (int) board[_i].size(); _j++)
+        {
+          find_solutions(board, count-1, _i, _j, total_sol, solutions);
+          if(count == 2)
+          {
+            _i = board.size();      // to break the nested loop when the returned
+            _j = board[_i].size();  // recursion call was the last one
+          }
+        }
+      }
+      board[i][j] = board[i-1][j];
+      board[i-1][j] = aux;
+    }
+    if(is_valid_right(board, i, j))
+    {
+      const int aux = board[i][j+1];
+      board[i][j+1] = board[i][j];
+      board[i][j] = 0;
+      for(int _i = 0; _i < (int) board.size(); _i++)
+      {
+        for(int _j = 0; _j < (int) board[_i].size(); _j++)
+        {
+          find_solutions(board, count-1, _i, _j, total_sol, solutions);
+          if(count == 2)
+          {
+            _i = board.size();      // to break the nested loop when the returned
+            _j = board[_i].size();  // recursion call was the last one
+          }
+        }
+      }
+      board[i][j] = board[i][j+1];
+      board[i][j+1] = aux;
+    }
+    if(is_valid_down(board, i, j))
+    {
+      const int aux = board[i+1][j];
+      board[i+1][j] = board[i][j];
+      board[i][j] = 0;
+      for(int _i = 0; _i < (int) board.size(); _i++)
+      {
+        for(int _j = 0; _j < (int) board[_i].size(); _j++)
+        {
+          find_solutions(board, count-1, _i, _j, total_sol, solutions);
+          if(count == 2)
+          {
+            _i = board.size();      // to break the nested loop when the returned
+            _j = board[_i].size();  // recursion call was the last one
+          }
+        }
+      }
+      board[i][j] = board[i+1][j];
+      board[i+1][j] = aux;
+    }
+    if(is_valid_left(board, i, j))
+    {
+      const int aux = board[i][j-1];
+      board[i][j-1] = board[i][j];
+      board[i][j] = 0;
+      for(int _i = 0; _i < (int) board.size(); _i++)
+      {
+        for(int _j = 0; _j < (int) board[_i].size(); _j++)
+        {
+          find_solutions(board, count-1, _i, _j, total_sol, solutions);
+          if(count == 2)
+          {
+            _i = board.size();      // to break the nested loop when the returned
+            _j = board[_i].size();  // recursion call was the last one
+          }
+        }
+      }
+      board[i][j] = board[i][j-1];
+      board[i][j-1] = aux;
+    }
+  }
+}
+
+int main()
+{
+  int rows, columns;
+  std::cin >> rows >> columns;
+
+  std::vector<std::vector<int>> board(rows, std::vector<int>(columns));
+  int count = 0;
+  fill_board(board, rows, columns, count);
+
+  int total_sol = 0;
+  std::set<std::vector<int>> solutions;
+  for(int i = 0; i < (int) board.size(); i++)
+  {
+    for(int j = 0; j < (int) board[i].size(); j++)
+    {
+      find_solutions(board, count, i, j, total_sol, solutions);
+    }
+  }
+
+  print_solutions(solutions, total_sol);
 
   return 0;
 }
